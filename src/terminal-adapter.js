c106e08d92cac24c81cc3867ee86f516b0e7e7be
tecfu <help@tecfu.com>
2017-06-06 07:30:14 -0700

    Removed tabs

--- src/terminal-adapter.js
+++ src/terminal-adapter.js
@@ -3,53 +3,53 @@
 let Smartwrap = require('./main.js');
 let yargs = require('yargs');
 yargs.option('paddingLeft', {
-	default: 0,
-	describe: 'Set the left padding of the output'
+  default: 0,
+  describe: 'Set the left padding of the output'
 });
 yargs.option('paddingRight', {
-	default: 0,
-	describe: 'Set the right padding of the output'
+  default: 0,
+  describe: 'Set the right padding of the output'
 });
 yargs.option('splitAt', {
-	default: [" ","\t"],
-	describe: 'Characters at which to split input'
+  default: [" ","\t"],
+  describe: 'Characters at which to split input'
 });
 yargs.option('trim', {
-	default: true,
-	describe: 'Trim the whitespace from end of input'
+  default: true,
+  describe: 'Trim the whitespace from end of input'
 });
 yargs.option('width', {
   alias: 'w',
-	default: 10,
+  default: 10,
   describe: 'Set the line width of the output (in spaces)',
   demandOption: true,
-	coerce:function(arg){
-		if(isNaN(arg*1)) {
-			throw new Error('Invalid width specified.');
-		}
-		return arg*1;
-	}
+  coerce:function(arg){
+    if(isNaN(arg*1)) {
+      throw new Error('Invalid width specified.');
+    }
+    return arg*1;
+  }
 });
 
 //create options object
 let options = {};
 [
-	'paddingLeft',
-	'paddingRight',
-	'splitAt',
-	'trim',
-	'width'
+  'paddingLeft',
+  'paddingRight',
+  'splitAt',
+  'trim',
+  'width'
 ].forEach(function(key){
-	if(typeof yargs.argv[key] !== undefined){
-		options[key] = yargs.argv[key]
-	}
+  if(typeof yargs.argv[key] !== undefined){
+    options[key] = yargs.argv[key]
+  }
 });
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(chunk) {
-	let out = Smartwrap(chunk,options);
-	console.log(out);
+  let out = Smartwrap(chunk,options);
+  console.log(out);
 });
 
 //yargs = yargs('h').argv;
