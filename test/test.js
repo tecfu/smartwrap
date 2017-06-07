c8b8536c3e502c5193726876b0ae3506ec48f0da
tecfu <help@tecfu.com>
2017-06-06 17:12:04 -0700

    Replaced tabs with spaces.

--- test/test.js
+++ test/test.js
@@ -8,50 +8,61 @@ var should = chai.should();
 var smartwrap = require('../src/main.js');
 var filepath = 'test/tests.json';
 var test = function(testResult,savedResult){
-	it('Strings should match',function(){
-		testResult.should.equal(savedResult);
-	})
+  it('Strings should match',function(){
+    testResult.should.equal(savedResult);
+  })
 };
 
 //get test list
 var str = fs.readFileSync(filepath,{
-	encoding : 'utf-8'
+  encoding : 'utf-8'
 });
 
 var obj = JSON.parse(str);
 
-for(var i in obj){	
-	
-	//generate new output 
-	var testResult = smartwrap(obj[i].input,{
-		width : obj[i].width
-	});
+for(var i in obj){  
+  
+  //generate new output 
+  let options = {};
+  [
+    'width',
+    'minWidth',
+    'paddingLeft',
+    'paddingRight',
+    'trim'
+  ].forEach(function(element){
+    if (typeof obj[i][element] !== 'undefined') {
+     options[element] = obj[i][element]; 
+    }
+  });
 
-	console.log("Test Properties:",obj[i]);
-	console.log("12345678901234567890");
-	console.log("BEGIN---------------");
-	console.log(testResult);
-	console.log("END-----------------\n");
-	switch(true){
-		case(typeof global.save !== 'undefined' && global.save):
-		//save tests
-			obj[i].output = testResult;
-			break;
-		case(typeof global.display !== 'undefined' && global.display):
-		//show tests (do nothing)
-			break;
-		default:
-		//run tests
-			describe('Test '+i,function(){
-				test(testResult,obj[i].output);
-			})
-	}
+  var testResult = smartwrap(obj[i].input,options);
+
+  console.log("Test Properties:",obj[i]);
+  console.log("12345678901234567890");
+  console.log("BEGIN---------------");
+  console.log(testResult);
+  console.log("END-----------------\n");
+  switch(true){
+    case(typeof global.save !== 'undefined' && global.save):
+    //save tests
+      obj[i].output = testResult;
+      break;
+    case(typeof global.display !== 'undefined' && global.display):
+    //show tests (do nothing)
+      break;
+    default:
+    //run tests
+      describe('Test '+i,function(){
+        test(testResult,obj[i].output);
+      })
+  }
 }
 
 if(typeof global.save !== 'undefined' && global.save){
-	//write saved object to file
-	fs.writeFileSync(filepath,JSON.stringify(obj,null,2),'utf8');
-	console.log("Tests saved to file.");
+  //write saved object to file
+  fs.writeFileSync(filepath,JSON.stringify(obj,null,2),'utf8');
+  console.log("Tests saved to file.");
 }
 
 module.exports = Tests;
