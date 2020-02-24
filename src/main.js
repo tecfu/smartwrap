28617624e2421e8389cefbdcda8dde4def11818e
Tec Fu <help@tecfu.com>
2020-02-24 17:32:40 -0500

    refactor: Clean config

--- src/main.js
+++ src/main.js
@@ -1,165 +1,161 @@
-let wcwidth = require("wcwidth")
-let breakword = require("breakword")
-
+const wcwidth = require("wcwidth")
+const breakword = require("breakword")
 
 const defaults = () => {
   let obj = {}
 
   obj.breakword = false
-  obj.calculateSpaceRemaining = function(obj) {
-    return Math.max(obj.lineLength - obj.spacesUsed - obj.paddingLeft - obj.paddingRight, 0)
-  } // function to set starting line length
-  obj.currentLine = 0 // index of current line in 'lines[]'
   obj.input = [] // input string split by whitespace
-  obj.lines = [
-    []
-  ] // assume at least one line
   obj.minWidth = 2 // fallback to if width set too narrow
   obj.paddingLeft = 0
   obj.paddingRight = 0
   obj.errorChar = "�"
   obj.returnFormat = "string" // or 'array'
   obj.skipPadding = false // set to true when padding set too wide for line length
-  obj.spacesUsed = 0 // spaces used so far on current line
   obj.splitAt = [" ", "\t"]
   obj.trim = true
   obj.width = 10
-  obj.words = []
 
   return obj
 }
 
 
+const calculateSpaceRemaining = function(lineLength, spacesUsed, config) {
+  return Math.max(lineLength - spacesUsed - config.paddingLeft - config.paddingRight, 0)
+} // function to set starting line length
+
+
 const validateInput = (text, options) => {
 
-  // text validation
-  let wText = text.toString()
-
-
   // options validation
-  let wConfig = Object.assign({}, defaults(), options || {})
+  let config = Object.assign({}, defaults(), options || {})
 
-  if (wConfig.errorChar) {
+  if (config.errorChar) {
     // only allow a single errorChar
-    wConfig.errorChar = wConfig.errorChar.split("")[0]
+    config.errorChar = config.errorChar.split("")[0]
 
     // errorChar must not be wide character
-    if (wcwidth(wConfig.errorChar) > 1)
-      throw new Error(`Error character cannot be a wide character (${wConfig.errorChar})`)
+    if (wcwidth(config.errorChar) > 1)
+      throw new Error(`Error character cannot be a wide character (${config.errorChar})`)
   }
 
   // make sure correct sign on padding
-  wConfig.paddingLeft = Math.abs(wConfig.paddingLeft)
-  wConfig.paddingRight = Math.abs(wConfig.paddingRight)
+  config.paddingLeft = Math.abs(config.paddingLeft)
+  config.paddingRight = Math.abs(config.paddingRight)
 
-  wConfig.lineLength = wConfig.width
-    - wConfig.paddingLeft
-    - wConfig.paddingRight
+  let lineLength = config.width
+    - config.paddingLeft
+    - config.paddingRight
 
-  if(wConfig.lineLength < wConfig.minWidth) {
+  if(lineLength < config.minWidth) {
     // skip padding if lineLength too narrow
-    wConfig.skipPadding = true
-    wConfig.lineLength = wConfig.minWidth
+    config.skipPadding = true
+    lineLength = config.minWidth
   }
 
   // to trim or not to trim...
-  if(wConfig.trim) {
-    wText = wText.trim()
+  if(config.trim) {
+    text = text.trim()
   }
 
-  return { wText, wConfig }
+  return { text, config, lineLength }
 }
 
 
-const wrap = (text, options) => {
+const wrap = (input, options) => {
 
-  let { wText, wConfig } = validateInput(text, options)
+  let { text, config, lineLength } = validateInput(input, options)
 
   // array of characters split by whitespace and/or tabs
-  let wordArray = []
+  let words = []
 
-  if(!wConfig.breakword) {
+  if(!config.breakword) {
     // break string into words
-    if(wConfig.splitAt.indexOf("\t")!==-1) {
+    if(config.splitAt.indexOf("\t")!==-1) {
       // split at both spaces and tabs
-      wordArray = wText.split(/ |\t/i)
+      words = text.split(/ |\t/i)
     } else{
       // split at whitespace
-      wordArray = wText.split(" ")
+      words = text.split(" ")
     }
   } else {
     // do not break string into words
-    wordArray = [wText]
+    words = [text]
   }
 
   // remove empty array elements
-  wConfig.words = wordArray.filter(val => {
+  words = words.filter(val => {
     if (val.length > 0) {
       return true
     }
   })
 
-  let spaceRemaining, splitIndex, word
+  // assume at least one line
+  let lines = [
+    []
+  ]
 
-  while(wConfig.words.length > 0) {
-    spaceRemaining = wConfig.calculateSpaceRemaining(wConfig)
-    word = wConfig.words.shift()
+  let spaceRemaining, splitIndex, word
+  let currentLine = 0 // index of current line in 'lines[]'
+  let spacesUsed = 0 // spaces used so far on current line
+
+  while(words.length > 0) {
+    spaceRemaining = calculateSpaceRemaining(lineLength, spacesUsed, config)
+    word = words.shift()
     let wordLength = wcwidth(word)
 
     switch(true) {
 
       // too long for an empty line and is a single character
-      case(wConfig.lineLength < wordLength && [...word].length === 1):
-        wConfig.words.unshift(wConfig.errorChar)
+      case(lineLength < wordLength && [...word].length === 1):
+        words.unshift(config.errorChar)
         break
 
         // too long for an empty line, must be broken between 2 lines
-      case(wConfig.lineLength < wordLength):
-        // break it, then re-insert its parts into wConfig.words
+      case(lineLength < wordLength):
+        // break it, then re-insert its parts into words
         // so can loop back to re-handle each word
-        splitIndex = breakword(word, wConfig.lineLength)
+        splitIndex = breakword(word, lineLength)
         let splitWord = [...word]
-        wConfig.words.unshift(splitWord.slice(0, splitIndex + 1).join(""))
-        wConfig.words.splice(1, 0, splitWord.slice(splitIndex + 1).join("")) // +1 for substr fn
+        words.unshift(splitWord.slice(0, splitIndex + 1).join(""))
+        words.splice(1, 0, splitWord.slice(splitIndex + 1).join("")) // +1 for substr fn
         break
 
       // not enough space remaining in line, must be wrapped to next line
       case(spaceRemaining < wordLength):
         // add a new line to our array of lines
-        wConfig.lines.push([])
+        lines.push([])
         // note carriage to new line in counter
-        wConfig.currentLine++
+        currentLine++
         // reset the spacesUsed to 0
-        wConfig.spacesUsed = 0
+        spacesUsed = 0
         /* falls through */
 
       // fits on current line
       // eslint-disable-next-line
       default:
         // add word to line
-        wConfig.lines[wConfig.currentLine].push(word)
+        lines[currentLine].push(word)
         // reduce space remaining (add a space between words)
-        wConfig.spacesUsed += wordLength + 1
+        spacesUsed += wordLength + 1
     }
   }
 
-  if(wConfig.returnFormat === "array") {
-    return wConfig.lines
-  } else {
-    let lines = wConfig.lines.map(function(line) {
-      // restore spaces to line
-      line = line.join(" ")
-      // add padding to ends of line
-      if(!wConfig.skipPadding) {
-        line = Array(wConfig.paddingLeft + 1).join(" ")
-          + line
-          + Array(wConfig.paddingRight + 1).join(" ")
-      }
-      return line
-    })
-    // return as string
-    return lines.join("\n")
-  }
+
+  lines = lines.map( line => {
+    // restore spaces to line
+    line = line.join(" ")
+    // add padding to ends of line
+    if(!config.skipPadding) {
+      line = Array(config.paddingLeft + 1).join(" ")
+        + line
+        + Array(config.paddingRight + 1).join(" ")
+    }
+    return line
+  })
+
+
+  return lines.join("\n")
 }
 
 
